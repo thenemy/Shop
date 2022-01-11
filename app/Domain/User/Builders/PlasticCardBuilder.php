c12f9da9f68060c078fe30d7a6320a1d29886126
maxwell <shaxa882@gmail.com>
2022-01-11 15:03:49 +0500

    filter fixed

--- app/Domain/User/Builders/PlasticCardBuilder.php
+++ app/Domain/User/Builders/PlasticCardBuilder.php
@@ -6,6 +6,25 @@ use App\Domain\Core\Main\Builders\BuilderEntity;
 
 class PlasticCardBuilder extends BuilderEntity
 {
+    public function joinUser(): PlasticCardBuilder
+    {
+        return $this->join("plastic_user_cards", "plastic_user_cards.plastic_id",
+            "=", "plastic_card.id");
+    }
+
+    public function joinUserWhere($user_id): PlasticCardBuilder
+    {
+        return $this->joinUser()->where("plastic_user_cards.user_id", '=', $user_id);
+    }
+
+    public function filterBy($filter)
+    {
+        if (isset($filter['user_id'])) {
+            $this->joinUserWhere($filter['user_id']);
+        }
+        return parent::filterBy($filter);
+    }
+
     public function byToken($token): PlasticCardBuilder
     {
         return $this->where('card_token', $token);
