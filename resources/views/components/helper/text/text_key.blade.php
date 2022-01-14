ce7d7a479ddaf4eae9d767a92c0dceaef5bf9596
maxwell <shaxa882@gmail.com>
2022-01-14 15:08:11 +0500

    fixed text_key

--- resources/views/components/helper/text/text_key.blade.php
+++ resources/views/components/helper/text/text_key.blade.php
@@ -1,4 +1,4 @@
 <div class="flex flex-row space-x-2">
-    <span class="font-bold">{{$key}} :</span>
-    <span>{{$value}}</span>
+    <span class="font-bold text-sm">{{$key}} :</span>
+    <span class="text-sm">{{$value}}</span>
 </div>
