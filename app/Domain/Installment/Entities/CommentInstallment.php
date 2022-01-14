f0832de06b6deb9e372857c1e8370da9718ae2a8
maxwell <shaxa882@gmail.com>
2022-01-14 14:27:54 +0500

    builder added

--- app/Domain/Installment/Entities/CommentInstallment.php
+++ app/Domain/Installment/Entities/CommentInstallment.php
@@ -3,9 +3,14 @@
 namespace App\Domain\Installment\Entities;
 
 use App\Domain\Core\Main\Entities\Entity;
+use App\Domain\Installment\Builders\CommentInstallmentBuilder;
 
 class CommentInstallment extends Entity
 {
     protected $table = "comment_installments";
 
+    public function newEloquentBuilder($query)
+    {
+        return new CommentInstallmentBuilder($query);
+    }
 }
