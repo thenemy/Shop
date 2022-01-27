9ddcd35c87b528630f42637344e88097a39adf6c
maxwell <shaxa882@gmail.com>
2022-01-27 11:45:24 +0500

    real price better

--- app/Domain/Product/Product/Logic/ProductLogic.php
+++ app/Domain/Product/Product/Logic/ProductLogic.php
@@ -35,9 +35,12 @@ class ProductLogic implements ProductInterface
     private function considerDiscounts($real_price)
     {
         if ($this->product->discounts()->exists()) {
-            $calculate_percent_price = ($this->product->percent
+            $calculate_percent_price = ($this->product->percentage
                     + $this->product->discounts()->sum("percent")) / 100 * $real_price;
             return $real_price - $calculate_percent_price;
+        }
+        if ($this->product->percentage) {
+            return $real_price - $this->product->percentage / 100 * $real_price;
         }
         return $real_price;
     }
